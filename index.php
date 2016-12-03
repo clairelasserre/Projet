d6cc459aad0e37cc10ea86510d38f74405a3db1e
leflemluc <leflemluc@gmail.com>
2016-12-03 17:25:50 +0100

    Update index
    Index modifié grâce au TD2

--- index.php
+++ index.php
@@ -1,33 +1,97 @@
-start_session()
 
--->
 <html>
-    <head>        
-    <link href="css/bootstrap.css" rel="stylesheet">  
-    <link href="css/perso.css" rel="stylesheet">  
+<!-- données spécifiques au référencement de la page ainsi que l'encodage utilisé -->
+
+<head>        
+    <?php
+    if (isset($_GET['name'])){
+        $askedPage = $_GET['name'];
+    }
+    else{
+        $askedPage = "Accueil";
+    }
+    
+    /* authorized est true si la page demandée est accessible*/
+    require("/Applications/XAMPP/xamppfiles/htdocs/PasDeGaspX/utils.php");
+
+    $authorized = checkPage($askedPage);
+    
+    
+    /* pageTitle contient le titre de la page qui apparaîtra en haut de page. pageMenuTitle contient le "title" présent dans le header*/
+
+    if ($authorized){
+        $pageTitle = getPageTitle($askedPage);
+        $pageMenuTitle = getPageMenuTitle($askedPage);
+        
+    }
+    else{
+        $pageTitle = 'Erreur';
+        $pageMenuTitle = 'Accueil';
+    }
+    
+    
+    
+    
+    generateHeader($pageMenuTitle,"css/perso.css");
+    ?>
 
 </head>
-    <body>
-    
-    <?php
 
-    session_name("TestTD4" );
-    session_start();
-    if (!isset($_SESSION['initiated'])) {
-        session_regenerate_id();
-        $_SESSION['initiated'] = true;
-    }
-    // Décommenter la ligne suivante pour afficher le tableau $_SESSION pour le debuggage
-     print_r($_SESSION);
 
-    /*require ("Utilisateurs.php");
-    require ("Offrir.php");
-    
-    echo "Votre plat s appelle ". $_POST['Titre'].", son aliment principal est ". $_POST['Ali'].", c'esr pour". $_POST['quantite']." personnes; on peut le consommer jusqu'à ".$_POST['date'];
+<!-- Ici, on va coder le menu, c'est à dire la partie du site que l'on voit sur toute les pages -->
+
+<body>
+     <body>
+       
         
-    */
-    ?>
-          <script src="js/jquery.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.js"></script>
+         
+        <div class="container">
+ 
+            <!-- Static navbar -->
+            <div class="navbar navbar-default" role="navigation">
+                <div class="container-fluid">
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php?name=Accueil">Accueil</a></li>
+                            <li class="active"><a href="index.php?name=Offrir">Offir un repas</a></li>
+                            <li><a href="index.php?name=PageInscription">S'inscrire</a></li>
+                            <li><a href="index.php?name=PageConnexion">Se connecter</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+ 
+            <div class="jumbotron">
+                <h1><?php
+                echo $pageTitle;
+                    ?>
+                </h1>
+                <p>Détails supplémentaires qui ne dépendent pas de la page</p>
+            </div>
+            
+            
+            
+            
+            <!-- Ici, on fera des appels aux différentes pages Web -->
+
+            
+            
+            <div id="content">
+                
+                <?php
+                require ("/Applications/XAMPP/xamppfiles/htdocs/PasDeGaspX/".$askedPage.".php");
+                ?>
+            </div>
+ 
+            <div id="footer">
+                <p>Site réalisé en Modal par…</p>
+            </div>
+ 
+        </div>
+</body>    
+
+
+<?php
+generateFooter();
+?>
 </html>
