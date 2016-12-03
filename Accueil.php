7f559226adbad44462fdde81b3dfb0869253a819
leflemluc <leflemluc@gmail.com>
2016-12-03 18:12:50 +0100

    Add files via upload

---
+++ Accueil.php
@@ -0,0 +1,25 @@
+<html>
+ 
+       
+
+        
+<div style="border: #686868 5px solid; padding: 10px; -moz-border-radius: 15px; -webkit-border-radius: 15px"> 
+    <div style="text-align: center"><img  src="cuisine .jpg" width="500" height="500" /></div>
+    <h3>
+Jacquelines : "Je ne gaspille plus"
+</h3>
+    <button type="button" class="btn btn-success"style="text-align: center;font-size: 1.5em"><a href="index.php?name=Offrir">Offrir un repas</a></button>
+</div>
+
+<div style="border: #686868 5px solid; padding: 10px; -moz-border-radius: 15px; -webkit-border-radius: 15px"> 
+    <div style="text-align: center"><img  src="Etre-en-pleine-forme-2.jpg" width="500" height="500" /></div>
+    <h3>
+Jean-Claude : "J'ai retrouvé le plaisir de bien diner"
+    </h3>
+     <button type="button" class="btn btn-success" style="text-align: center; font-size: 1.5em"> Consulter les offres près de chez moi  </button>
+</div>
+
+ 
+
+
+</html>
---
+++ NotreAction.php
@@ -0,0 +1,31 @@
+
+<html>
+ 
+
+ 
+   <body>
+
+        
+        
+
+<h1>Le gaspillage en France</h1>
+<br></br>
+<h2>Le gaspillage en France gaspille 2000kg de blbaahudiuezbacuyp erhv rehvi rzjf mijzeoifjezf v    g cui hxjpoze hcgearuimocjrkabuhrhujruihfujerhabfjiocjebhrjfiojhbejifohbuijohbijojhbgvuiuhgvuijtyuiytyuiuytresdfghjkhgf</h2>
+<br></br>
+<br></br>
+<h1>Notre histoire</h1>
+<br></br>
+
+<h2>Le gaspillage en France gaspille 2000kg de blbaahudiuezbacuyp erhv rehvi rzjf mijzeoifjezf v    g cui hxjpoze hcgearuimocjrkabuhrhujruihfujerhabfjiocjebhrjfiojhbejifohbuijohbijojhbgvuiuhgvuijtyuiytyuiuytresdfghjkhgf</h2>
+<br></br>
+<br></br>
+<h1>Pas de Gaspix, comment ca marche ? </h1>
+<br></br>
+<h2>Le gaspillage en France gaspille 2000kg de blbaahudiuezbacuyp erhv rehvi rzjf mijzeoifjezf v    g cui hxjpoze hcgearuimocjrkabuhrhujruihfujerhabfjiocjebhrjfiojhbejifohbuijohbijojhbgvuiuhgvuijtyuiytyuiuytresdfghjkhgf</h2>
+<br></br>
+</font></div> 
+    <button type="button" class="btn btn-success"style="text-align: center"> Pret à offrir, en 1 clic propose un repas </button>
+</div>
+
+</html>
+
