806c89d51fe203db4138c48ed5e30737e9ceabae
leflemluc <leflemluc@gmail.com>
2016-12-04 14:57:52 +0100

    Add files via upload

---
+++ ApresSEtreInscritAvecEchec.php
@@ -0,0 +1,8 @@
+<html>
+    
+    <h1>Mais vous pouvez dès maintenant...</h1>
+    <button type='button' class='btn btn-success' style='text-align: center; font-size: 1.5em'><a href='index.php?name=PageInscription'>Retournez à la page d inscription</a></button>;
+
+</html>
+  
+
---
+++ ApresSEtreInscritAvecSucces.php
@@ -0,0 +1,18 @@
+<html>
+    
+    
+    <h1>Bravo, vous êtes à présent inscrit!!</h1>
+    
+    <br></br>
+    
+    <button type="button" class="btn btn-success" style="text-align: center; font-size: 1.5em"><a href="index.php?name=Offrir">Offrez dès maintenant!!</a></button>
+    
+    <h2>Ou bien...</h2>
+
+    <br></br>
+    
+    <button type="button" class="btn btn-success" style="text-align: center; font-size: 1.5em"><a href="index.php?name=ListeOffres">Trouver un repas près de chez vous!</a></button>   
+
+    
+</html>    
+
