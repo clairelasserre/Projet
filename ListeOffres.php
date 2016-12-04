95fb0616240a0c13fc692201c7e5c67428f3c645
leflemluc <leflemluc@gmail.com>
2016-12-04 11:55:09 +0100

    First draft required before wednsday

--- ListeOffres.php
+++ ListeOffres.php
@@ -1,8 +1,2 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+On peut s'occuper de cette page avant mercredi et faire une première ébauche!
 
