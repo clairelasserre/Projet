1bc62354c1eb53c3c6e10d732c5d6d8528d43e61
leflemluc <leflemluc@gmail.com>
2016-12-04 12:39:33 +0100

    Gestion Session required
    La page index a sa structure globale. Il faut maintenant y rajouter les histoires de sessions.

--- index.php
+++ index.php
@@ -1,94 +1,75 @@
-
 <html>
-<!-- données spécifiques au référencement de la page ainsi que l'encodage utilisé -->
 
-<head>        
-    <?php
-    if (isset($_GET['name'])){
-        $askedPage = $_GET['name'];
-    }
-    else{
-        $askedPage = "Accueil";
-    }
-    
-    /* authorized est true si la page demandée est accessible*/
-    require("/Applications/XAMPP/xamppfiles/htdocs/PasDeGaspX/utils.php");
+    <head>        
+        <?php
+        if (isset($_GET['name'])) {
+            $askedPage = $_GET['name'];
+        } else {
+            $askedPage = "Accueil";
+        }
 
-    $authorized = checkPage($askedPage);
-    
-    
-    /* pageTitle contient le titre de la page qui apparaîtra en haut de page. pageMenuTitle contient le "title" présent dans le header*/
+        /* authorized est true si la page demandée est accessible */
+        require("/Applications/XAMPP/xamppfiles/htdocs/PasDeGaspX/Utilitaires/utils.php");
 
-    if ($authorized){
-        $pageTitle = getPageTitle($askedPage);
-        $pageMenuTitle = getPageMenuTitle($askedPage);
+        $authorized = checkPage($askedPage);
+
+
+        /* pageTitle contient le titre de la page qui apparaîtra en haut de page. pageMenuTitle contient le "title" présent dans le header */
+
+        if ($authorized) {
+            $pageTitle = getPageTitle($askedPage);
+            $pageMenuTitle = getPageMenuTitle($askedPage);
+        } else {
+            $pageTitle = 'Erreur';
+            $pageMenuTitle = 'Accueil';
+        }
+
+
+
+
+        generateHeader($pageMenuTitle, "css/perso.css");
+        ?>
+
+    </head>
+
+
+
+    <body>
         
-    }
-    else{
-        $pageTitle = 'Erreur';
-        $pageMenuTitle = 'Accueil';
-    }
-    
-    
-    
-    
-    generateHeader($pageMenuTitle,"css/perso.css");
-    ?>
+    <!-- Ici, on va coder le menu, c'est à dire la partie du site que l'on voit sur toute les pages -->
 
-</head>
+            <?php
+            generateMenu($pageTitle);
+            ?>
+  
 
 
-<!-- Ici, on va coder le menu, c'est à dire la partie du site que l'on voit sur toute les pages -->
 
-<body>
-     <body>
-       
-        
-         
-        <div class="container">
- 
-            <!-- Static navbar -->
-            <div class="navbar navbar-default" role="navigation">
-                <div class="container-fluid">
-                    <div class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
-                            <li><a href="index.php?name=Accueil">Accueil</a></li>
-                            <li class="active"><a href="index.php?name=Offrir">Offir un repas</a></li>
-                            <li><a href="index.php?name=PageInscription">S'inscrire</a></li>
-                            <li><a href="index.php?name=PageConnexion">Se connecter</a></li>
-                        </ul>
-                    </div>
+                <!-- Ici, on fera des appels aux différentes pages Web -->
+
+
+
+                <div id="content">
+
+<?php
+/* La page demandée n'est ouverte que si elle fait parties des pages possibles! */
+if ($authorized) {
+    require ("/Applications/XAMPP/xamppfiles/htdocs/PasDeGaspX/ContenuDesPages/" . $askedPage . ".php");
+} else {
+    echo 'Erreur 404';
+}
+?>
                 </div>
-            </div>
- 
-            <div class="jumbotron">
-                <h1><?php
-                echo $pageTitle;
-                    ?>
-                </h1>
-                <p>Détails supplémentaires qui ne dépendent pas de la page</p>
-            </div>
-            
-            
-            
-            
-            <!-- Ici, on fera des appels aux différentes pages Web -->
+                <br></br>
+                <br></br>
+                <br></br>
+               
+                <div id="footer">
+                    <p>Site réalisé en Modal par Luc&Claire</p>
+                </div>
 
-            
-            
-            <div id="content">
-                
-                <?php
-                require ("/Applications/XAMPP/xamppfiles/htdocs/PasDeGaspX/".$askedPage.".php");
-                ?>
             </div>
- 
-            <div id="footer">
-                <p>Site réalisé en Modal par…</p>
-            </div>
- 
-        </div>
-</body>    
+    </body>    
 
 
 <?php
