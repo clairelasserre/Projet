8efeff34f343e171264a2b92a52c9b07aa2c9083
leflemluc <leflemluc@gmail.com>
2016-12-04 12:31:27 +0100

    Update utils.php

--- utils.php
+++ utils.php
@@ -23,6 +23,27 @@ $page_list = array(
     "name"=>"PageConnexion",
     "title"=>"Vous êtes déjà membres, connectez-vous!",
     "menutitle"=>"Se connecter"),
+    array(
+    "name"=>"NotreAction",
+    "title"=>"Apprenez notre action",
+    "menutitle"=>"Descriptif action"
+    ),
+    array(
+    "name"=>"ListeOffres",
+    "title"=> "Consulter les offers disponibles",
+    "menutitle"=>"Liste des offres",
+    ),
+    array(
+    "name"=>"ApresAvoirOffert",
+    "title"=> "Merci pour votre offre!",
+    "menutitle"=>"Offre enregistrée",   
+    ),
+    array(
+    "name"=>"ApresSEtreInscrit",
+    "title"=> "Vous êtes désormais inscrit!",
+    "menutitle"=>"Membre enregistré",    
+    )
+   
 );  
 
 
@@ -89,6 +110,7 @@ CHAINE_DE_FIN;
 
 
 /*Génère les footer*/
+
 function generateFooter(){
 echo <<<CHAINE_DE_FIN
     <script src="js/jquery.js"></script>        
@@ -98,8 +120,47 @@ CHAINE_DE_FIN;
    
 }
 
- 
 
+
+/*Genère le menu*/
+
+function generateMenu($titre){
+echo <<<CHAINE_DE_FIN
+<nav class="navbar navbar-light bg-faded">
+    <a class="navbar-brand" href="index.php?name=Accueil">PAS DE GASPIX</a>
+    <ul class="nav navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="index.php?name=NotreAction">Notre action <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Les offres</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php?name=Offrir">Faire un don</a>
+    </li>
+    <li class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Mon profil
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="#">Voir mon profil </a></li>
+    <li><a href="#">Se déconnecter</a></li>
+  </ul>
+</li>
+
+
+
+    
+  </ul>
+  
+</nav>
+            <div class="jumbotron">
+                <h1>
+CHAINE_DE_FIN;
+                    echo $titre;
+                echo'</h1>';
+            echo'</div>';
+
+}
 
 ?>
 
