8a581cb92daaaa923f41f46bdee728d5983f8766
leflemluc <leflemluc@gmail.com>
2016-12-04 12:13:27 +0100

    Layout required + possibility to add aliments
    Le form est complet, à cela prêt qu'il faut ajouter une fonctionnalité permettant de rajouter un aliment principal dans le descriptif.
    La mise en page reste à faire.

--- Offrir.php
+++ Offrir.php
@@ -1,21 +1,18 @@
-
-
 <html>
-    
-<head>        
-    <link href="css/bootstrap.css" rel="stylesheet">  
-    <link href="css/perso.css" rel="stylesheet">  
-
-</head>     
+        
 
     <h1>OFFRIR</h1> 
 
     <h2>Ce que je propose</h2>
     
         <h3> Caractéristiques de votre plat </h3>
-        
-    <form action="index.php" method="post">
+    
+    <!-- Ci-dessous, le form à compléter pour enregistrer une offre. 
+    Une fois validée, l'offre est enregistrée, et on est redirigée vers la page ApresAvoirOffert -->
+    
+    <form action="index.php?name=ApresAvoirOffert" method="post">
 
+    
     <div class="row">
         <div class="col-md-2 gris titre">Titre du plat</div>
         <div class="col-md-2 gris titre"><input type="text" name="Titre" required/></div>
@@ -23,7 +20,7 @@
     
     <div class="row">
         <div class="col-md-2 gris titre">Jusqu'à quand peut-on consommer?</div>
-        <div class="col-md-2 gris titre"><input type="date" name="date" required="C'est mieux de ne pas donner quelque chose de périmé"/></div>
+        <div class="col-md-2 gris titre"><input type="date" name="Date" required="C'est mieux de ne pas donner quelque chose de périmé"/></div>
     </div>
         
     <div class="row">
@@ -47,9 +44,10 @@
     <div class="row">
         <div class="col-md-2 gris titre">Quel est l'élément principal de votre plat?</div>
         <div class="col-md-2 gris titre">  
-            <?php    
-                require ("AlimentsExistant.php");
-                require ("Database.php");
+            <?php 
+                /*Fait apparaître la liste, par ordre alphabétique, des aliments exitsants*/
+                require ("ClassesAssocieesBDD/AlimentsExistant.php");
+                require ("ClassesAssocieesBDD/Database.php");
                 $dbh = Database::connect();
                 AlimentsExistant::scrollAliment($dbh);
                 $dbh = null;
@@ -64,14 +62,15 @@
     </div>
     
         
+        
         <h3>Lieu de la rencontre</h3>
         
+               
     <div class="row">
         <div class="col-md-2 gris titre">Indiquer l'adresse de la rencontre</div>
         <div class="col-md-2 gris ">Rentrer votre adresse<input type="text" name="Adresse" required/></div>
         <div class="col-md-1 gris "></div>
         <div class="col-md-2 gris ">Rentrer votre code postal<input type="int" name="CodePostal" required/></div>    
-
     </div>
     
     <div class="row">
@@ -80,7 +79,7 @@
     </div>
         
     <input type="submit" value="Valider" />
-    `
+    
     </form>
         
         
@@ -96,7 +95,5 @@
 
 
 
-       <script src="js/jquery.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.js"></script>
+
 </html>
